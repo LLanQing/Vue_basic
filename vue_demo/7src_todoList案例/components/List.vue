6ebed0bcdd467b738dc6723fdc80422e81f6b6d4
LLanQing <44639217+LLanQing@users.noreply.github.com>
2022-07-26 16:03:57 +0800

    todoList案例

---
+++ vue_demo/7src_todoList案例/components/List.vue
@@ -0,0 +1,44 @@
+<template>
+    <ul class="todo-main">
+    <Item 
+      v-for="todo in todos"
+      :key="todo.id" 
+      :todo="todo" 
+      :handleCheck="handleCheck"
+      :handleDelete="handleDelete"
+    />
+  </ul>
+</template>
+
+<script>
+import Item from './Item'
+export default {
+  name:'List',
+  //注册Item组件
+  components:{
+    Item
+  },
+  //声明接受todos
+  props:['todos','handleCheck','handleDelete']
+
+}
+</script>
+
+<style scoped>
+  /*main*/
+  .todo-main {
+    margin-left: 0;
+    border: 1px solid #ddd;
+    border-radius: 2px;
+    padding: 0px;
+  }
+
+  .todo-empty {
+    height: 40px;
+    line-height: 40px;
+    border: 1px solid #ddd;
+    border-radius: 2px;
+    padding-left: 5px;
+    margin-top: 10px;
+  }
+</style>
