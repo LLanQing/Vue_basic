9bb5f972716b79d68fd0a2523923aa2798240b07
LLanQing <44639217+LLanQing@users.noreply.github.com>
2022-07-26 20:31:36 +0800

    todoList组件自定义事件

---
+++ vue_demo/11src_todoList组件自定义事件/App.vue
@@ -0,0 +1,114 @@
+<template>
+  <div id="root">
+    <div class="todo-container">
+      <div class="todo-wrap">
+        <MyHeader @addTodo="addTodo"/>
+        <List :todos="todos" :handleCheck="handleCheck" :handleDelete="handleDelete"/>
+        <MyFooter :todos="todos" @checkAllTodo="checkAllTodo" @clearCheckAll="clearCheckAll"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import MyHeader from './components/MyHeader' 
+import List from './components/List'
+import MyFooter from './components/MyFooter'
+
+export default {
+  //注册子组件
+  components:{
+    MyHeader,
+    List,
+    MyFooter
+  },
+    data(){
+    return {
+      todos:[
+        {id:'001',title:'睡觉',done:true},
+        {id:'002',title:'吃饭',done:false},
+        {id:'003',title:'学习',done:false},
+      ]
+    }
+  },
+  methods:{
+    // 添加todo
+    addTodo(todoObj){
+      console.log('App组件里的addTodo被调用了'+todoObj);
+      //将接收到的用户数据添加到数组的开头
+      this.todos.unshift(todoObj);
+    },
+    // 修改todo.done
+    handleCheck(id){
+      this.todos.forEach((todo) => {
+        if(todo.id  === id){
+          todo.done = !todo.done
+        }
+      })
+    },
+    //删除todo
+    handleDelete(id){
+      //过滤掉要删除的todo
+      this.todos = this.todos.filter((todo) =>todo.id !== id)
+    },
+    // 全选或取消全选
+    checkAllTodo(done){
+      this.todos.forEach((todo) => { 
+        todo.done = done;
+       })
+    },
+    //清除所有已完成
+    clearCheckAll(){
+      this.todos = this.todos.filter((todo) => {
+        return !todo.done
+      })
+    }
+  }
+
+}
+</script>
+
+<style >
+  /*base*/
+  body {
+    background: #fff;
+  }
+
+  .btn {
+    display: inline-block;
+    padding: 4px 12px;
+    margin-bottom: 0;
+    font-size: 14px;
+    line-height: 20px;
+    text-align: center;
+    vertical-align: middle;
+    cursor: pointer;
+    box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05);
+    border-radius: 4px;
+  }
+
+  .btn-danger {
+    color: #fff;
+    background-color: #da4f49;
+    border: 1px solid #bd362f;
+  }
+
+  .btn-danger:hover {
+    color: #fff;
+    background-color: #bd362f;
+  }
+
+  .btn:focus {
+    outline: none;
+  }
+
+  .todo-container {
+    width: 600px;
+    margin: 0 auto;
+  }
+  .todo-container .todo-wrap {
+    padding: 10px;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+  }
+</style>
