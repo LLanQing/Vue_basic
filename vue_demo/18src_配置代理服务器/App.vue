19121265d869079979e05b7d848f58155e1e7460
LLanQing <44639217+LLanQing@users.noreply.github.com>
2022-07-27 20:17:59 +0800

    配置代理

---
+++ vue_demo/18src_配置代理服务器/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <button @click="getMsg">点击获取信息</button>
+  </div>
+</template>
+
+<script>
+// 引入axios
+import axios from 'axios'
+
+export default {
+  name:'App',
+
+  methods:{
+    getMsg(){
+      axios.get('/js').then(  //这里开启了代理服务器，请求被转发到了5500端口
+        (response) => {
+          console.log('请求成功了',response.data);
+        },
+        (error) => {
+          console.log('请求失败了',error.message);  //会报错，跨域问题,使用vue-cli解决跨域
+        }
+      )
+    }
+  }
+}
+</script>
+
+<style >
+
+</style>
--- vue_demo/vue.config.js
+++ vue_demo/vue.config.js
@@ -1,5 +1,32 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
 	transpileDependencies: true,
+	// 关闭代码保存校验
 	lintOnSave: false,
+	//开启代理服务器(方法一)
+	devServer: {
+		proxy: "http://localhost:5500", //将请求转发给端口号5500，注意这里只能配置单个代理，不能配置多个代理
+	},
+	/* 	//开启代理服务器(方式二) 多个代理
+	devServer: {
+		proxy: {
+			// 当请求的前缀是api，直接转发请求到服务器5500端口
+			"/api": {
+				target: "http://localhost:5500",
+				ws: true, //用于支持websocket
+				changeOrigin: true, //用于控制请求头中host的值
+				pathRewrite: {
+					//把请求中含有的api替换成空字符串
+					"^/api": "",
+				},
+			},
+			"/demo": {
+				target: "http://127.0.0.1:5500",
+				//不写 ws和changeOrigin默认为true
+				pathRewrite: {
+					"^/demo": "",
+				},
+			},
+		},
+	}, */
 });
