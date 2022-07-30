b341242f661e9e72a9bf5b24e5bed9051819e894
LLanQing <44639217+LLanQing@users.noreply.github.com>
2022-07-30 18:05:41 +0800

    命名路由name和params参数

---
+++ vue_demo/26src_命名路由name和params参数/pages/Detail.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    {{$route.params.title}}
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Detail'
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue_demo/26src_命名路由name和params参数/pages/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h2>我是Home内容</h2>
+    <div>
+      <ul class="nav nav-tabs">
+        <li>
+          <!-- to的路径注意要带上/home -->
+          <router-link class="list-group-item" active-class="active" to="/home/news">News</router-link>
+        </li>
+        <li>
+          <router-link class="list-group-item" active-class="active" to="/home/messages">Message</router-link>
+        </li>
+      </ul>
+      <router-view>
+      </router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Home',
+  mounted(){
+    console.log('Home组件挂载完毕');
+  },
+  destroyed(){
+    console.log('Home组件被销毁了');
+  }
+}
+</script>
+
+<style>
+
+</style>
