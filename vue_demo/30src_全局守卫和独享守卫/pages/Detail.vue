98ebc37eb1a48522e0668cfb18ce9964c02cf17b
LLanQing <44639217+LLanQing@users.noreply.github.com>
2022-08-01 10:06:49 +0800

    全局守卫和独享守卫

---
+++ vue_demo/30src_全局守卫和独享守卫/pages/Detail.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    {{title}}
+    <hr>
+    <!-- {{a}} -->
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Detail',
+  props:['id','title']
+}
+</script>
+
+<style>
+
+</style>
