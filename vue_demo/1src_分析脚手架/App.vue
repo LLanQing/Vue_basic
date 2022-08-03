2f3a22aea3f7c73b3d3c4677382944f939d57aef
LLanQing <44639217+LLanQing@users.noreply.github.com>
2022-08-03 17:32:18 +0800

    分析脚手架生成的文件

--- vue_demo/1src_分析脚手架/App.vue
+++ vue_demo/1src_分析脚手架/App.vue
@@ -9,6 +9,8 @@
 import School from './components/School'; //.vue后缀可写可不写
 import Student from './components/Student';
 
+// Vue.extend(options) 可简写为： options ，vue自动帮我们调用Vue.extend()
+//使用默认暴露将组件构造函数VueComponent暴露出去
 export default {
   //注册Student,School组件
   components:{
